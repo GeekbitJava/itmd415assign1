840027d637469cf24ccdc286ca962dc06a075dde
geekbitcreations <dbarndt@hawk.iit.edu>
2018-01-30 20:13:49 -0600

    cleaning up documentation

--- 415Assignment1Server/src/calculatorClient.java
+++ 415Assignment1Server/src/calculatorClient.java
@@ -1,3 +1,12 @@
+/**
+ * Deborah Barndt
+ * Thomas Boller
+ * 1-30-18
+ * calculatorClient.java
+ * Assignment 1
+ * This program will 
+ * Written by Deborah Barndt & Thomas Boller. */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -11,68 +20,69 @@ import javax.xml.ws.Response;
 
 public class calculatorClient {
 	
-	public static void main(String[] args){
-		
-		// Testing my commits
+	public static void main(String[] args)
+	{
 		// Declarations of local variables
 	    Socket clientSock; 
 	    int port = 5556;
 	    String server = "localhost";
 	   
-	    //try and catch block to open communication via the Socket sock1
+	    // Try and catch block to open communication via the Socket sock1
 	    try {
-	    	
 	    		//creates instance of clientSock at localhost port 5555
-	           clientSock = new Socket(server, port);
+	           	clientSock = new Socket(server, port);
 	           
-	           //BufferedReader will take an InputStreamReader which takes our sockets input stream
-	           //This will be how we will take data in from the socket
-	           BufferedReader clientReader = new BufferedReader (
+	           	// BufferedReader will take an InputStreamReader which takes our sockets input stream
+	           	// This will be how we will take data in from the socket
+	           	BufferedReader clientReader = new BufferedReader (
 	        		   new InputStreamReader(clientSock.getInputStream()));
 
-	           //printwriter takes data from client and sends to server as byte stream
-	           PrintWriter clientWriter =
+	           	// PrintWriter takes the data from client and sends to server as byte stream
+	           	PrintWriter clientWriter =
 	        	        new PrintWriter(clientSock.getOutputStream(), true);
 	           
 	           
-	           //Scanner KeyScan will take the users input from the keyboard
-	           Scanner keyScan = new Scanner(System.in);
+	           	// Scanner KeyScan will take the users input from the keyboard
+	           	Scanner keyScan = new Scanner(System.in);
 	           
-	           //Some variables to hold user input and response from server
-	           String equation, result;
+	           	// Some variables to hold user input and response from server
+	            String equation, result;
 	           
-	           //this shows a proper connection is made with the server
-	          String response = clientReader.readLine();
-	          System.out.println(response);
+	            // This will display a proper connection is made with the server
+	            String response = clientReader.readLine();
+	            System.out.println(response);
 	          
 
- 	           //prompt the user and receive input
-	          System.out.println("Welcome to the 415 Calculator.");
-	          System.out.println("You may enter \"count\" to show the number of connections,");
-	          System.out.println("or \"exit\" to disconnect.");
+ 	           	// Prompt the user to enter input and receive result
+	            System.out.println("Welcome to the 415 Calculator.");
+	            System.out.println("You may enter \"count\" to show the number of connections,");
+	            System.out.println("or \"exit\" to disconnect.");
 
 	           
-	           //do while loop keeps the prompt going until the user enters exit
-	           do{
-
+	            // Do while loop keeps the prompt going until the user enters exit
+	           do
+	           {
 		           System.out.println("Enter simple equation: \n");
 		           equation = keyScan.nextLine();
 		           
-		           //send the user's equation to the server
+		           // Send the user's equation to the server
 		           clientWriter.println(equation);
 		           
-		           //if the equation is not exit then it will show what was received from the server
-		           if(!equation.equals("exit")){
+		           // If the input is not exit, then it will show what was received from the server
+		           if(!equation.equals("exit"))
+		           {
 		        	   result = clientReader.readLine();
 		        	   System.out.println(result);
-		           }
-		           
-		           
-	        	   //if exit is enter will exit this loop
-	           }while(!equation.equals("exit"));
+		           }           
+	           }
+	           
+	           // If exit is entered it will exit this loop
+	           while(!equation.equals("exit"));
 	           
 	    }
-	    catch (IOException e) {
+	    
+	    catch (IOException e) 
+	    {
 	        System.out.println("Client side error: " + e.getMessage());
 	    }
 
